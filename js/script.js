8d62eecfeebb428bdb1b181469636e56bd923440
lemanpashayeva <lmnpasayeva@gmail.com>
2022-06-09 18:19:55 +0400

    exercises were compleated

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@
 
 /* function Button(){
     alert("Leman Pashayeva");
-};
-   Button() */
+ }; */
 
 //2. hisse :
 
-/*  var amount = prompt("2reqemli bir eded daxil edin")
-    function moneyLeft(){
-        alert("100-amount")
-    }
-*/
+/*  var amount = prompt("2reqemli bir eded daxil edin:") ;
+  function moneyLeft() {
+      alert(100-amount);
+  };
+  moneyLeft(); */
